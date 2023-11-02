adbe9eb3fd6c90445ddcb951a3c518358d34b6f4
Flown <105561081+super-miner@users.noreply.github.com>
2023-11-01 20:04:54 -0700

    Commit before large change

--- Scripts/UI/ProfileListEntry.cs
+++ Scripts/UI/ProfileListEntry.cs
@@ -9,17 +9,26 @@ public partial class ProfileListEntry : ItemListEntry {
     
     public new string Name = "";
 
+    private StoredProfile _profile;
+
     public void FromProfile(StoredProfile profile) {
-        SetName(profile.Name);
+        _profile = profile;
+        
+        SetName(_profile.Name, false);
     }
 
     public void Serialize() {
         GD.Print($"Serialized profile {Name}.");
-        StoredDataManager.Data.Profiles.Add(new StoredProfile(Name));
+        _profile = new StoredProfile(Name);
+        StoredDataManager.Data.Profiles.Add(_profile);
     }
 
-    public void SetName(string name) {
+    public void SetName(string name, bool updateProfile = true) {
         Name = name;
+
+        if (updateProfile) {
+            _profile.Name = Name;
+        }
 
         if (Name != "") {
             _nameText.Text = Name;
