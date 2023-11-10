fa8741bc7b63aa9385a2dfe6c56a89bd7465b25e
Flown <105561081+super-miner@users.noreply.github.com>
2023-11-10 09:37:03 -0800

    Fixed various errors

--- Scripts/UI/Buttons/AddProfileButton.cs
+++ Scripts/UI/Buttons/AddProfileButton.cs
@@ -4,14 +4,14 @@ using CoreLauncher.Scripts.UI.Generic;
 namespace CoreLauncher.Scripts.UI.Buttons;
 
 public partial class AddProfileButton : Button {
-	[Export] private ProfileList itemList = null;
+	[Export] private ProfileList itemList;
 	
 	public void OnPressed() {
 		if (itemList != null) {
 			itemList.AddEntry();
 		}
 		else {
-			GD.Print("Could not add profile because the itemList was not found.");
+			GD.PrintErr("Add Profile Button: Could not add profile because the itemList was not found.");
 		}
 	}
 }
