3ba6700170e5f5a9cc04751943eaa6a86fef1e67
super-miner <theredmugproductions@gmail.com>
2023-11-11 17:38:42 -0800

    Removed commands that were not being used

--- Commands/SteamCMD/Login.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-set path=%~1
-set path=%path:__= %
-"%path%\steamcmd" +login %~2 %~3 +quit
--- Commands/SteamCMDTest.bat
+++
@@ -1,6 +0,0 @@
-rem "%~1\steamcmd" +login super_miner_1 +app_update 1621690 +app_run 1621690 +quit
-
-@echo hello
-@echo hello
-
-pause
