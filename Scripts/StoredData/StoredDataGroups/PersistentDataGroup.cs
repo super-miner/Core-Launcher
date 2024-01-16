f9aa7afd640f337d77526d0329de91531266cdf3
super-miner <theredmugproductions@gmail.com>
2024-01-15 21:35:11 -0800

    Added a version to the configs for future use

--- Scripts/StoredData/StoredDataGroups/PersistentDataGroup.cs
+++ Scripts/StoredData/StoredDataGroups/PersistentDataGroup.cs
@@ -3,6 +3,7 @@ using System.Text.Json.Serialization;
 namespace CoreLauncher.Scripts.StoredData.StoredDataGroups; 
 
 public class PersistentDataGroup : StoredDataGroup {
+    [JsonInclude] public int ConfigVersion = 1;
     [JsonInclude] public bool SetupComplete = false;
     [JsonInclude] public string SteamExePath = "";
     [JsonInclude] public string SteamGamesPath = "";
