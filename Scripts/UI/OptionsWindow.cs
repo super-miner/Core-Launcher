7cce952d663d29f9848ac0641924440eeeb00247
super-miner <theredmugproductions@gmail.com>
2024-11-28 21:40:03 -0800

    Various bug fixes

--- Scripts/UI/OptionsWindow.cs
+++ Scripts/UI/OptionsWindow.cs
@@ -1,4 +1,3 @@
-using CoreLauncher.Scripts.Menus.Main;
 using CoreLauncher.Scripts.StoredData;
 using CoreLauncher.Scripts.Systems;
 using CoreLauncher.Scripts.UI.Generic;
@@ -13,11 +12,15 @@ public partial class OptionsWindow : Window
 	[Export] private FileLineEdit _serverPathLineEdit;
 	[Export] private FileLineEdit _appDataPathLineEdit;
 
-	public override void _Ready() {
-		_steamPathLineEdit.SetText(GameManager.SteamExePath);
-		_gamePathLineEdit.SetText(GameManager.SteamGamePath);
-		_serverPathLineEdit.SetText(GameManager.SteamGameServerPath);
-		_appDataPathLineEdit.SetText(GameManager.AppDataPath);
+	public void Toggle() {
+		Visible = !Visible;
+
+		if (Visible) {
+			_steamPathLineEdit.SetText(GameManager.SteamExePath);
+			_gamePathLineEdit.SetText(GameManager.SteamGamePath);
+			_serverPathLineEdit.SetText(GameManager.SteamGameServerPath);
+			_appDataPathLineEdit.SetText(GameManager.AppDataPath);
+		}
 	}
 
 	public void OnCloseRequested() {
@@ -28,18 +31,22 @@ public partial class OptionsWindow : Window
 
 	public void Save() {
 		if (_steamPathLineEdit.PathIsValid(out string _)) {
+			GD.Print("Settings Menu: Saving Steam path.");
 			GameManager.SteamExePath = _steamPathLineEdit.Text;
 		}
 
 		if (_gamePathLineEdit.PathIsValid(out string _)) {
+			GD.Print("Settings Menu: Saving Client path.");
 			GameManager.SteamGamePath = _gamePathLineEdit.Text;
 		}
 
 		if (_serverPathLineEdit.PathIsValid(out string _)) {
+			GD.Print("Settings Menu: Saving Server path.");
 			GameManager.SteamGameServerPath = _serverPathLineEdit.Text;
 		}
 
 		if (_appDataPathLineEdit.PathIsValid(out string _)) {
+			GD.Print("Settings Menu: Saving App Data path.");
 			GameManager.AppDataPath = _appDataPathLineEdit.Text;
 		}
 
